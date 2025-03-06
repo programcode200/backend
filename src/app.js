e697da834e47b6934b60f59ee194407bed33a3b8
programcode200 <rohans1021@gmail.com>
2025-03-06 14:26:13 +0530

    cors error fix

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const app = express();
 //use, use for middleware and configuration settings
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    // origin: process.env.CORS_ORIGIN,
+    origin: "https://frontend-youtube-kappa.vercel.app",
     credentials: true,
   })
 );
